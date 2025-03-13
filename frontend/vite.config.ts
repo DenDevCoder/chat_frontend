3a534b958b471e6f7940ecf10c1c243f30df7128
Denis Steckij <steckijdenis12@gmail.com>
2025-03-13 16:30:27 +0300

    project init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_PORT || 3000),
+    },
+  };
+});
